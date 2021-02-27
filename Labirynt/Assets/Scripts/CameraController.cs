9a2bb2a2495319597e4a797337b201f8015b5a89
gzelek <50342912+gzelek8@users.noreply.github.com>
2021-02-27 10:20:41 +0100

    PlayerController

---
+++ Labirynt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensivity = 100f;
+    public Transform palyerBody;
+    public float XRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        palyerBody = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        XRotation -= mouseY; // xRotation = xRotation - mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+        palyerBody.Rotate(Vector3.up * mouseX);
+    }                      
+}
