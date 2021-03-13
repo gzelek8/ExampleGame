0bcf0664edb341bafc6c10a342fe9f4b7794a80a
esiu <50342912+gzelek8@users.noreply.github.com>
2021-03-13 10:21:22 +0100

    Terrain Penalty

---
+++ Labirynt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public int timeToEnd;
+    bool endGame = false;
+    bool gamePaused = false;
+    void Start()
+    {
+        if (gameManager == null)
+        {
+            gameManager = this;
+        }
+        if (timeToEnd <= 0)
+        {
+            timeToEnd = 30;
+        }
+        InvokeRepeating("Stopper", 2, 1);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+           if(gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+        
+    }
+
+    public void EndGame()
+    {
+        CancelInvoke("Stopper");
+        Debug.Log("End time");
+
+    }
+    void Stopper()
+    {
+        timeToEnd--;
+        Debug.Log("Time to end: " + timeToEnd + " s");
+        if ( timeToEnd <= 0)
+        {
+            timeToEnd = 0;
+            endGame = true;
+        }
+        if (endGame)
+        {
+            EndGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Debug.Log("Pause game");
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    void ResumeGame()
+    {
+        Debug.Log("Resume game");
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+}
--- Labirynt/Assets/Scripts/PlayerController.cs
+++ Labirynt/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     public float gravaity = -10f;
     Vector3 velocity;
     CharacterController characterController;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,41 @@ public class PlayerController : MonoBehaviour
 
        velocity.y += gravaity * Time.deltaTime;
        characterController.Move(velocity * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(
+                groundCheck.position,
+                transform.TransformDirection(Vector3.down),
+                out hit,
+                0.4f,
+                groundMask))
+        {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag;
+            switch (terrainType)
+            {
+                default:
+                    speed = 12;
+                    break;
+                case "LowPlane":
+                    speed = 3;
+                    break;
+                case "HighPlane":
+                    speed = 20;
+                    break;
+            }
+        }
+    }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "PickUp")
+        {
+            hit.gameObject.GetComponent<PickUp>().Picked();
+        }
     }
 }
