870f2885a688deab7e17895006d35bc7e97338c7
esiu <50342912+gzelek8@users.noreply.github.com>
2021-03-27 10:05:18 +0100

    add  muzzle

---
+++ Labirynt/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float healthPoints = 100f;
+    public void TakeDamage(float damage)
+    {
+        healthPoints -= damage;
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Labirynt/Assets/Scripts/Weapon.cs
+++ Labirynt/Assets/Scripts/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
 {
     public Camera FPScamera;
     public float range = 100f;
+    public ParticleSystem muzzleFLash;
+    public float damage = 25f;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -17,12 +19,30 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        ProcessRaycast();
+        PlayMuzzleFlash();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFLash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        Physics.Raycast(
+        if (Physics.Raycast(
                         FPScamera.transform.position,
                         FPScamera.transform.forward,
                         out hit,
-                        range);
-        Debug.Log(hit.transform.name);
+                        range))
+        {
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null)
+            {
+                return;
+            }
+            target.TakeDamage(damage);
+        }
     }
 }
